eb7e6a909688e264a31bf069dc0c8f4174402db0
sudhanshu <ramansudhanshu150@gmail.com>
2017-10-23 01:46:16 +0530

    it is an image

---
+++ main.py
@@ -0,0 +1,16 @@
+''' this is for converting a imagein to a text file '''
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+'''im = Image.open("temp.jpg") # the second one 
+im = im.filter(ImageFilter.MedianFilter())
+enhancer = ImageEnhance.Contrast(im)
+im = enhancer.enhance(2)
+im = im.convert('1')
+im.save('temp2.jpg')'''
+file = open("book.txt",'w') 
+text = pytesseract.image_to_string(Image.open('book.jpg'))
+text = text.decode('unicode_escape').encode('utf-8')
+file.write(text);
+file.close();
+#print(text)
