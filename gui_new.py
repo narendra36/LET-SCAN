93556a97389fa9dc0faaac2c346166f54fe03563
narendra36 <dodwaria.nd536@gmail.com>
2017-11-10 14:23:16 +0530

    dialog box added while no file selected

--- gui_new.py
+++ gui_new.py
@@ -1,5 +1,5 @@
 import  sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 import qt_designer 
 from PyQt5 import QtGui
 from createPdf import create
@@ -12,13 +12,20 @@ class MainWindow(QMainWindow):
         self.ui.convertToPdf.clicked.connect(self.convertPrograss)
         self.ui.browse.clicked.connect(self.openFileNameDialog)
         self.ui.progressBar.setValue(0)
+        self.path = []
 
     def convertPrograss(self):
+        self.ui.label.setText('Processing...')
         self.completed = 0
+        if not self.path:
+            QMessageBox.about(self, 'No File Selected', 'Please Select atleast One File')
+            self.ui.label.setText('Progress :')
+            return        
         create(self.path)
         while self.completed < 100:
             self.completed += 0.0001
             self.ui.progressBar.setValue(self.completed)
+        self.ui.label.setText('Completed')
 
     def openFileNameDialog(self):    
         options = QFileDialog.Options()
@@ -27,7 +34,7 @@ class MainWindow(QMainWindow):
         if fileName[0]:
             self.path = fileName
             self.ui.filePath.setText(fileName[0]+' , '+ str(len(fileName)) + ' images selected')
-            print(self.path)    
+            print(self.path)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
